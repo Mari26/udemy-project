a7a5200ac69d8478c4a4b15be5b17e9d35a0cd3d
mari <mari.qubro@gmail.com>
2023-05-14 00:02:28 +0400

    update apps.js file

--- assets/scripts/apps.js
+++ assets/scripts/apps.js
@@ -2,15 +2,21 @@ const defaultResult = 0;
 
 let currentResult = defaultResult;
 
+// Gets input from input field
 function getUserNumberInput(){
 return parseInt(userInput.value);
 }
 
+function createAndWriteOutput(operator, resultBeforeCalc, CulcNumber){
+    const calcDescreption =`${resultBeforeCalc} ${operator} ${CulcNumber}`;
+    outputResult (currentResult,calcDescreption); 
+}
+
 function add(){
     const enteredNumber = getUserNumberInput();
-    const calcDescreption =`${currentResult} + ${enteredNumber}`;
-    currentResult = currentResult + enteredNumber;
-    outputResult (currentResult,calcDescreption);    
+    const initialResult = currentResult;
+    currentResult += enteredNumber;
+    createAndWriteOutput ('+',initialResult,enteredNumber); 
 }
 
 /*
@@ -18,7 +24,34 @@ currentResult = add (2,4);
 let description = `${currentResult} + "სრული სიგიჟე"`
 outputResult (currentResult,'bbgb');*/
 
+function subtract (){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteOutput ('-',initialResult,enteredNumber);  
+}
+
+function multiply (){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteOutput ('*',initialResult,enteredNumber);  
+}
+
+
+function divide (){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+    createAndWriteOutput ('/',initialResult,enteredNumber); 
+}
+
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
 
 
 
